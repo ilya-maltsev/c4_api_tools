8c959e6a77f1f974e7bb86cfec1d08923e3b38a3
Ilya Maltsev <i.y.maltsev@yandex.ru>
2026-03-19 13:43:11 +0300

    urllib_version

--- c4_lib-2.0/setup.py
+++ c4_lib-2.0/setup.py
@@ -3,7 +3,7 @@ setup(name = 'c4_lib',
     version = '2.0',
     py_modules = ['c4_lib'],
     packages = ['c4_lib'],
-    install_requires = ['urllib3', 'requests'],
+    install_requires = ['urllib3<2', 'requests'],
     include_package_data = True,
     package_data = {'': ['openssl.cnf', 'gost.so']}
 )
